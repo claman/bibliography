c77ddcdd84014cd6278e326b733ad614ae758017
Alex Claman <claman@alexclaman.com>
2016-01-03 15:42:21 -0800

    currently broken, but a start

--- cruncher.py
+++ cruncher.py
@@ -5,7 +5,10 @@ file = open('biblio.txt', 'r')
 authorSearch = re.compile('^[\D]*\D')
 pubDateSearch = re.compile('\d{4}\.')
 titleSearch = re.compile('(?<=\d{4}\. )(.*(\."|\. ))')
+journalTitleSearch = re.compile('(?<="\s)(.*)(?=:)')
+journalPagesSearch = re.compile('(?:\:\s*)(\d*-\d*)')
 containingVolumeSearch = re.compile('(?<=( In )).*(?=\d)')
+    # for this pull out the pages at the end \d*-\d*\.
 
 for line in file:
     author = authorSearch.match(line)
@@ -24,17 +27,47 @@ for line in file:
     if title[0] == '"':
         title = title[1:]
         form = '@article'
-    else:
-        form = '@book'
+    elif title[0] != '"':
+        format = '@book'
+    # if title[0] != '"':
+    #     format = '@book'
+    # else:
+    #     title = title[1:]
+    #     switcher = 'true'
 
-    containingVolume = containingVolumeSearch.search(line)
-    if containingVolume:
-        containingVolume = containingVolume.group()
+    # if switcher == 'true':
+    #     journalPages = journalPagesSearch.search(line)
+    #     containingVolume = containingVolumeSearch.search(line)
+    #     if journalPages:
+    #         journal = journalTitleSearch.search(line)
+    #         journal = journal.group()
+    #         pages = journalPages
+    #         format = '@article'
+    #     elif containingVolume:
+    #         containingVolume = containingVolume.group()
+    #         format = '@excerpt'
 
-    print form + '{'
-    print ' author: "' + author + '",'
-    print ' year: "' + pubDate + '",'
-    print ' title: "' + title + '",'
-    print '}'
-    print
-    print containingVolume
+    if format == '@book':
+        print format + '{'
+        print ' author: "' + author + '",'
+        print ' year: "' + pubDate + '",'
+        print ' title: "' + title + '",'
+        print '}'
+        print
+    if format == '@article':
+        print format + '{'
+        print ' author: "' + author + '",'
+        print ' year: "' + pubDate + '",'
+        print ' title: "' + title + '",'
+        print ' journal: "' + journal + '",'
+        print ' pages: "' + pages + '"'
+        print '}'
+        print
+    # elif format == '@excerpt':
+    #     print format + '{'
+    #     print ' author: "' + author + '",'
+    #     print ' year: "' + pubDate + '",'
+    #     print ' title: "' + title + '",'
+    #     print '}'
+    #     print
+    #     print containingVolume
